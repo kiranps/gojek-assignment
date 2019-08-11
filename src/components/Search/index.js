737e4876b4ab083563946827d07483ab9a8a4968
Kiran PS <pskirann@gmail.com>
2019-08-11 07:39:38 +0530

    add Search

---
+++ src/components/Search/index.js
@@ -0,0 +1,49 @@
+import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
+import styles from "./styles.module.css";
+
+const Search = () => {
+  const inputDiv = useRef(null);
+  const inputRef = useRef(null);
+  const [value, setValue] = useState("");
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleChange = e => {
+    const text = e.target.value;
+    if (text.length > 1 && text.slice(-1) === " ") {
+      setValue("");
+      setTags([...tags, text.slice(0, -1)]);
+    } else {
+      setValue(text);
+    }
+  };
+
+  const width = inputDiv.current
+    ? inputDiv.current.offsetWidth + 10 + "px"
+    : "10px";
+
+  return (
+    <div className={styles.search}>
+      {tags.map((x, i) => (
+        <div key={i} className={styles.keyword}>
+          {x}
+        </div>
+      ))}
+      <div ref={inputDiv} className={styles.hidden}>
+        {value}
+      </div>
+      <input
+        value={value}
+        ref={inputRef}
+        className={styles.input}
+        onChange={handleChange}
+        style={{ width }}
+      />
+    </div>
+  );
+};
+
+export default Search;
