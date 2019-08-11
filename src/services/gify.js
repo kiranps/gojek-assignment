6e6c381174749f50263d1b0aa3b4734defe71c4e
Kiran PS <pskirann@gmail.com>
2019-08-11 10:50:11 +0530

    add hook for gifs

--- src/services/gify.js
+++ src/services/gify.js
@@ -1,9 +1,10 @@
+import { useState, useEffect } from "react";
 const DOMAIN = "https://api.giphy.com/";
 const KEY = "upRAglSKfcxmYqFMCWGOlS1SXoxnyLIF";
 
-export const search = query => {
+const search = (query, offset = 0) => {
   const ENDPOINT = "v1/gifs/search";
-  const QUERY_PARAMS = `?api_key=${KEY}&q=${query}&limit=25&offset=0&rating=G&lang=en`;
+  const QUERY_PARAMS = `?api_key=${KEY}&q=${query}&limit=25&offset=${offset}&rating=G&lang=en`;
 
   return fetch(DOMAIN + ENDPOINT + QUERY_PARAMS)
     .then(response => response.json())
@@ -12,7 +13,21 @@ export const search = query => {
       data.map(x => ({
         id: x.id,
         title: x.title,
-        image: x.images.downsized
+        image: x.images.downsized_still
       }))
     );
+};
+
+export const useGifs = () => {
+  const [gifs, setGifs] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  const fetchGifs = query => {
+    search(query, offset).then(data => {
+      setGifs(images => [...images, ...data]);
+      setOffset(offset => offset + 1);
+    });
+  };
+
+  return [gifs, fetchGifs];
 };
