8df018222a12e4cf322cf1eec6d8deb578b0913b
Kiran PS <pskirann@gmail.com>
2019-08-12 17:26:35 +0530

    lint fix

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./styles.module.css";
 import { ThemeConsumer } from "context/ThemeContext";
 import * as R from "ramda";
